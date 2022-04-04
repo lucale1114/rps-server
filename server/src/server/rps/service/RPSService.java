64bbeb7ec43efff224d8e427f2dd8f8db0213f08
Eleni Solomon-Samuelsson <elesol2001@skola.goteborg.se>
2022-04-04 14:30:08 +0200

    removed unused imports

--- server/src/server/rps/service/RPSService.java
+++ server/src/server/rps/service/RPSService.java
@@ -3,13 +3,6 @@ package server.rps.service;
 import java.util.List;
 
 import javax.ejb.Local;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 
 import server.rps.entity.Player;
 
