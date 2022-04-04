9f11de7aa9529e705a7c955a2e097e93ce76e4a2
Luca A <lucale1114@skola.goteborg.se>
2022-04-04 14:30:53 +0200

    very important fix and new names

--- server/src/server/rps/dataaccess/PlayerDataAccessProductionVersion.java
+++ server/src/server/rps/dataaccess/PlayerDataAccessProductionVersion.java
@@ -17,21 +17,18 @@ public class PlayerDataAccessProductionVersion implements PlayerDataAccess {
 	@PersistenceContext
 	private EntityManager em;
 	
-	@Override
 	public List<Player> retrievePlayers() {
 		Query q = em.createQuery("select player from Player player");
 		List<Player> players = q.getResultList();
 		return players;
 	}
 
-	@Override
 	public Player retrievePlayerByUsername(String username) {
 		Query q = em.createQuery("select player from Player player where player.username = :username");
 		q.setParameter("username", username);
 		return (Player) q.getSingleResult();
 	}
 
-	@Override
 	public void registerPlayer(Player newPlayer) {
 		em.persist(newPlayer);
 	}
