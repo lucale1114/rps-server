4195dbc3901bc4d389fe145cc450ef6c9dcbfa47
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-04-04 15:09:56 +0200

    Added Player class in client

---
+++ client/src/client/Player.java
@@ -0,0 +1,54 @@
+package client;
+
+public class Player {
+
+	private int id;
+	private String username;
+	private String password;
+	private int wins;
+	private int defeats;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	public int getDefeats() {
+		return defeats;
+	}
+
+	public void setDefeats(int defeats) {
+		this.defeats = defeats;
+	}
+
+	public String toString() {
+		return "Player (" + this.id + ") - " + this.username;
+	}
+}
