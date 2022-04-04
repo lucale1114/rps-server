92cd060d50a10471368e48e4fc589fde53da04a2
Luca A <lucale1114@skola.goteborg.se>
2022-04-04 11:55:40 +0200

    Added entity and rest

---
+++ server/src/server/rps/entity/Player.java
@@ -0,0 +1,70 @@
+package server.rps.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String username;
+	private String password;
+	private int wins;
+	private int defeats;
+	
+	public Player() {}
+
+	public Player(String username, String password, int wins, int defeats) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.wins = wins;
+		this.defeats = defeats;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	public int getDefeats() {
+		return defeats;
+	}
+
+	public void setDefeats(int defeats) {
+		this.defeats = defeats;
+	}
+	
+	
+}
---
+++ server/src/server/rps/rest/Rest.java
@@ -0,0 +1,45 @@
+package server.rps.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import server.rps.entity.Player;
+
+
+@Stateless
+@Path("/players")
+public class Rest {
+	
+ //	@Inject
+ // private RPSService service;
+	
+	@GET
+	@Produces("application/JSON")
+	public List<Player> getPlayers(){
+		return service.getPlayers();
+	}
+	
+	@GET
+	@Produces("application/JSON")
+	@Path("{playerName}")
+	public Player getPlayer(@PathParam("playerName") String username){
+		return service.getPlayer(username);
+	}
+	
+	@POST
+	@Produces("application/JSON")
+	@Consumes("application/JSON")
+	public Player registerPlayer(Player player) {
+		service.registerPlayer(player);
+		return player;
+	}
+	
+}
